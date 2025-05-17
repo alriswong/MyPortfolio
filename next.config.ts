a570f734e69eef67524eeba027f3e7e1c7c9e0f7
alriswong <101098149+alriswong@users.noreply.github.com>
2025-05-17 20:04:17 +0800

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isPord = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isPord ? "/MyPortfolio" : "",
   transpilePackages: ['three'],
   devIndicators: false,
   output: 'export',
