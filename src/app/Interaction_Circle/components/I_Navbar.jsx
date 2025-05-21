2a0c8251c7b62a55a74cf3707520380f55d800f5
alriswong <101098149+alriswong@users.noreply.github.com>
2025-05-21 11:56:44 +0800

    bt effect changed

--- src/app/Interaction_Circle/components/I_Navbar.jsx
+++ src/app/Interaction_Circle/components/I_Navbar.jsx
@@ -16,13 +16,13 @@ const navbar = () => {
   return (
     <nav className='fixed mx-auto border border-[#33353F] border-l-transparent border-r-transparent top-0 left-0 right-0 z-50 bg-black bg-opacity-100'>
       <div className='group flex flex-row justify-start px-4 py-4 md:px-10 xl:px-20 2xl:px-30 '>
-        <Link href='/#portfolio' className="flex flex-row items-center text-white gap-3 duration-175 hover:text-2xl focus:text-2xl max-2xl:active:text-BTcolorOne group/link">
+        <Link href='/#portfolio' className="flex flex-row items-center text-white gap-3 duration-175 hover:text-2xl focus:text-2xl max-2xl:active:bg-BTcolorOne/50 group/link">
         
             <motion.div
             initial = {{x:-8 , opacity:0.5}}
             animate={{x:0, opacity:1}}
             transition={{duration: 1.5 , ease: ["easeInOut"], repeat: Infinity}} 
-            className="text-[#c4c4c4] max-2xl:group-active/link:text-BTcolorOne">
+            className="text-[#c4c4c4]">
                 <ArrowCircleLeft size={32} />
             </motion.div>
         
