5910b2bd07483d2c03e844ebec43ec7c02bce508
alriswong <101098149+alriswong@users.noreply.github.com>
2025-05-26 22:20:03 +0800

    updated head

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -6,6 +6,7 @@ const Head = () => {
     <>
         <title>Alris Wong Portfolio</title>
         <meta name="description" content="Alris Wong - Game Developer, Gameplay Designer, base in Hong Kong and Taiwan" />
+        <meta name="google-site-verification" content="googlec24e65c1da5916f6.html"></meta>
     </>
   )
 }
