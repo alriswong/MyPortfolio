429f926b4b4d6870f398502ad02da6650df7678c
alriswong <101098149+alriswong@users.noreply.github.com>
2025-05-26 21:59:30 +0800

    Updated Head

---
+++ src/app/head.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+        <title>Alris Wong Portfolio</title>
+        <meta name="description" content="Alris Wong - Game Developer, Gameplay Designer, base in Hong Kong and Taiwan" />
+    </>
+  )
+}
+
+export default Head
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Head from 'next/head';
 import HeroSection from"./components/HeroSection";
 import ScrollIcon from "./components/ScrollIcon"
 import Navbar from "./components/Navbar";
@@ -12,7 +12,9 @@ import Footer from"./components/Footer";
 
 export default function Home() {
   return (
+    
     <main className="flex flex-col bg-black overflow-clip">
+
       <Navbar />
       <div className="container mx-auto mt-15 md:mt-28 px-8 md:px-10 lg:px-20 ">
         <HeroSection />
