f650664fe11889ecbc7dc5bbb4fa1b3c63dcabcf
alriswong <101098149+alriswong@users.noreply.github.com>
2025-05-26 23:00:49 +0800

    updated head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,10 @@ export const metadata = {
   
   title: 'Alris Wong Portfolio',
   description: 'Alris Wong - Game Developer, Gameplay Designer, based in Hong Kong and Taiwan',
-  'google-site-verification': 'nR2GWzX_rCIvu6k2q_1ZCexIePS_RiVpeXvGsoZUDaM',
   
+  verification: {
+    google: 'nR2GWzX_rCIvu6k2q_1ZCexIePS_RiVpeXvGsoZUDaM',
+  },
 }
 
 
